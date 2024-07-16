617a6fb2caa2388280fd361eff002c5a245495f2
cristofher13 <145610867+cristofher13@users.noreply.github.com>
2024-07-16 11:28:35 -0500

    Add files via upload

---
+++ POLLERIA DIAZ/paginacentral.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style3pagina.css">
+    <link rel="icon" type="image/png" href="img/icon.ico">
+    <!-- Link de iconos -->
+    <script src="https://kit.fontawesome.com/f368768ce7.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
+    <title>DIAZ</title>
+</head>
+<header>
+    <header>
+        <div class="centrarimg">
+            <div class="frase">
+                Bienvenido a Pollería Díaz <br> 
+            </div>
+            <div class="frased">
+                ¡Disfruta de la mejor comida de la región!<br>
+                En Pollería Díaz nos orgullece ofrecerte pollos<br>
+                a la brasa fresco y delicioso, preparado con nuestras<br>
+                recetas tradicionales<br>
+            </div>
+        <nav>
+            <ul class="menuenlace">
+                <li><a href="#delivery" style="font-size: 2em;">Delivery</a></li>
+                <li><a href="#menu" style="font-size: 2em;">Menú</a></li>               
+                <li><a href="https://www.google.com/maps/place/Poller%C3%ADa+D%C3%ADaz/@-12.0422013,-77.2584482,10z/data=!4m10!1m2!2m1!1spolleria+diaz!3m6!1s0x9105b9eb264aea4d:0x9fb384fcb06465d9!8m2!3d-12.211055!4d-76.9451992!15sCg1wb2xsZXJpYSBkaWF6Wg8iDXBvbGxlcmlhIGRpYXqSARJjaGlja2VuX3Jlc3RhdXJhbnTgAQA!16s%2Fg%2F11rylgd692?entry=ttu" style="font-size: 2em;">Ubicación</a></li>
+                <li><a href="#contacto" style="font-size: 2em;">Contacto</a></li>
+                <li><img src="img/POLLO LOGO.png" alt="imagen"></li>
+
+            </ul>
+        </nav>
+
+    </header>
+    <main>
+        <section id="inicio">
+            <h2>Bienvenido a Pollería Diaz</h2>
+            <p>¡Disfruta de la mejor comida de la región! En Pollería Diaz nos enorgullece ofrecerte pollo a la brasa fresco y delicioso, preparado con nuestras recetas tradicionales.</p>
+        </section>
+        <section id="menu">
+            <h2>Nuestro Menú</h2>
+            <div class="menucatalogo">
+            <img src="https://polleriaslagranja.com/wp-content/uploads/2022/10/La-Granja-Real-Food-Chicken-1-Pollo-a-la-Brasa.png" alt="plato1"width="300" class="tamañopollo">       <img src="https://polleriaslagranja.com/wp-content/uploads/2022/10/La-Granja-Real-Food-Chicken-Promo-Delivery-1.png" alt="plato2"width="300" class="tamañopollo">       <img src="https://polleriaslagranja.com/wp-content/uploads/2022/10/La-Granja-Real-Food-Chicken-1.2-Pollo-a-la-Brasa.png" alt="plato3"width="300" class="tamañopollo"><br></br>
+            </div>
+            <p>Aquí encontrarás una selección de nuestros platos más populares:</p>
+            <ul>
+                <li>Pollo a la brasa</li>
+                <li>Pollo frito</li>
+                <li>Alitas de pollo</li>
+                <li>Pollo broaster</li>
+                <li>Ensaladas frescas</li>
+            </ul>
+        </section>
+
+            <a href="https://www.facebook.com/PollosYParrillasDiaz/" target="_blank">
+                <i class="fa-brands fa-facebook"></i>
+            </a>
+
+        </section>
+    </main>
+      <!-- ............................................................ -->
+
+      <footer>
+        <div class="footer-container">
+            <div class="footer-section">
+
+                <h3>SERVICIOS</h3> 
+                <p><strong>Dirección:</strong></p>
+                     📍Av. Velasco Sector 2 GP 23 MZ "F" LT  22 Villa el Salvador<BR>
+                     📍AV. MICAELA VASTIDAS 1666 - RUTA B  Villa el Salvador</p>
+                <p><strong>DELIVERY & TAKE OUT</strong></p>
+                <p>Lunes a Domingos 11:30 a.m. a 10:00 p.m.</p>
+                <p><strong>PIDE LLAMANDO AL: <BR>901505855 - 921004541
+                     </strong></p>
+            </div>
+               
+            <div class="footer-section">
+
+            <h3>NOSOTROS</h3>       
+            <p><a href="postulacion.php">¡Postula aquí!</a></p>
+            <p><a href="historia.html">Nuestra Historia</a></p>
+            <p><a href="https://www.google.com/maps/place/Poller%C3%ADa+D%C3%ADaz/@-12.0422013,-77.2584482,10z/data=!4m10!1m2!2m1!1spolleria+diaz!3m6!1s0x9105b9eb264aea4d:0x9fb384fcb06465d9!8m2!3d-12.211055!4d-76.9451992!15sCg1wb2xsZXJpYSBkaWF6Wg8iDXBvbGxlcmlhIGRpYXqSARJjaGlja2VuX3Jlc3RhdXJhbnTgAQA!16s%2Fg%2F11rylgd692?entry=ttu">Locales</a></p>
+            </div>
+            <div class="footer-section">
+
+                <h3>MÉTODO DE PAGO</h3>
+                <img src="img/bcp.png" alt="bcp" class="bcp">
+                <img src="img/yape.png" alt="yape">
+                <p>Tarjetas de crédito,débito y Yape online</p>
+            </div>
+            <div class="footer-section">
+
+            <h3>MAS</h3>
+                <p>CHMM S.A.C. - 20606663227</p>
+                <p>Política de Privacidad</p>
+                <p>Protocolo Pet Friendly</p>
+                <p>Libro de Reclamaciones</p>
+                <p>Atención al Cliente</p>
+                <p>Tel. 921-004-541</p>
+                <a href="libro.php">
+        <img src="/img/libronegro.png" alt="Libro de Reclamaciones" class="libro">
+    </a>
+                </div>
+       
+            </div>
+        </div>
+    </footer>
+    <div class="footer-bottom">
+            <p class="derechos">@POLLERIA DIAZ'S 2024 todos los derechos reservados</p>
+        </div>
+
+
+</body>
+</html>
